c61760f974f0f29f422b861049a879323dbeb5cd
yehezkiel ermanto <yehezkiel.ermanto28@gmail.com>
2023-08-30 11:29:17 +0700

    feat: add view in client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'client';
+
+
+export class AppComponent implements OnInit {
+  title = 'Client';
+  message: string = '';
+
+  constructor(private cdr: ChangeDetectorRef){};
 
   ngOnInit() {
     window.Echo.channel('hello-channel').listen('HelloEvent', (e: any) => {
-      console.log(e);
+      this.message = e.message;
+      this.cdr.detectChanges();
     });
   }
 }
